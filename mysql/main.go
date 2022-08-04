c333ab6fad496b3b4b22ca8f7f435571dba422a9
CarterPlus <hashcat@126.com>
2022-08-04 11:40:52 +0800

    feat: 原生SQL实战

--- mysql/main.go
+++ mysql/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"database/sql"
 	"fmt"
+
+	//导入mysql驱动包"
+	_ "github.com/go-sql-driver/mysql"
 )
-import _ "github.com/go-sql-driver/mysql" //导入mysql驱动包"
 
 type User struct {
 	Id   int
@@ -12,21 +14,27 @@ type User struct {
 }
 
 func main() {
-	db, err := sql.Open("mysql", "root:howie@tcp(127.0.0.1:3306)/test?charset=utf8")
+	db, err := sql.Open("mysql", "root:123456@tcp(127.0.0.1:3306)/test?charset=utf8")
 	if err != nil {
 		panic(err)
 	}
+
 	var user []User
-	rows, err := db.Query("select * from  user")
+	rows, err := db.Query("select * from user")
+	if err != nil {
+		panic(err)
+	}
+
 	for rows.Next() {
 		var Id int
 		var Name string
+
 		if err := rows.Scan(&Id, &Name); err != nil {
 			continue
 		}
 		user = append(user, User{Id: Id, Name: Name})
-
 	}
+
 	fmt.Println(user)
-	//[{1 111111} {2 22222} {3 33333} {4 44444}]
+	// [{1 红红的太阳在东边升起} {2 迷人的小花蕊}]
 }
